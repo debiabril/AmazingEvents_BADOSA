aeb552e9b1e719c0e681e2cb7c104f36771c1f71
debiabril <debora.badosa@gmail.com>
2023-03-13 12:44:46 -0300

    pagina de details mostrando segun el id funcionando

--- scripts/functions.js
+++ scripts/functions.js
@@ -60,21 +60,40 @@ function displayEvents(events, container, renderFunction, ruta) {
     renderCards(events, container, renderFunction, ruta);
 }
 
-function createDetailsCard(id) {
+function createDetailsCard(event, container) {
     let div = document.createElement('div');
-    div.classList='card mx-2 my-2';
+    div.classList='row g-0';
     div.innerHTML=`
-        <img src="${event.image}" class="card-img-top" alt="${event.category}">
-        <div class="card-body text-center">
-            <h5 class="card-title">${event.name}</h5>
-            <p class="card-text">${event.description}</p>
-            <div class="d-flex justify-content-between">
-                <p class="pt-2">Price: $${event.price}</p>
-                <a href="${ruta}details.html?id=${event._id}" class="btn btn-nav align-self-center go">Go to Details</a>
+    <div class="col-md-6 d-flex justify-content-center align-items-center">
+        <img src="${event.image}" id="cardImg" class="img-fluid borderRadius " alt="${event.name}">
+    </div>
+    <div class="col-md-6">
+        <div class="card-body">
+            <dl class="card-text">
+                <dt>Name</dt>
+                <dd>${event.name}</dd>
+                <dt>Date</dt>
+                <dd>${event.date}</dd>
+                <dt>Description</dt>
+                <dd>${event.description}</dd>
+                <dt>Category</dt>
+                <dd>${event.category}</dd>
+                <dt>Place</dt>
+                <dd>${event.place}</dd>
+                <dt>Capacity</dt>
+                <dd>${event.capacity}</dd>
+                <dt>${event.assistance ? "Asistence" : "Estimate"}</dt>
+                <dd>${event.assistance ? event.assistance : event.estimate}</dd>
+                <dt>Price</dt>
+                <dd>${event.price}</dd>
+            </dl>
+            <div class="d-flex justify-content-end">
+                <a href="../index.html" class="btn btn-details align-self-center go">Back to Home</a>
             </div>
-        </div>`;
-    return div;
+        </div>
+    </div>`;
+    return container.appendChild(div)
 }
 
 
-export { createCard, renderCards, displayEvents, showCategoriesInCheckboxes, };
+export { createCard, renderCards, displayEvents, showCategoriesInCheckboxes, createDetailsCard};
